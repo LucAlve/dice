d9c797121fc048d1c643d9c6cd515f252a77460a
LucAlve <71749837+LucAlve@users.noreply.github.com>
2021-10-13 23:55:45 -0500

    new JS

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ var randomNumber = Math.floor(Math.random() * 6) + 1
 var secondRandomNumber = Math.floor(Math.random() * 6) + 1
 
 document.getElementsByClassName("img1")[0].setAttribute("src", "images/dice"+randomNumber+".png");
-document.getElementsByClassName("img2")[0].setAttribute("src", "images/dice"+secondRandomNumber+".png");
+
+// professors way
+document.querySelectorAll("img")[1].setAttribute("src", "images/dice"+secondRandomNumber+".png");
 
 if (randomNumber > secondRandomNumber) {
-  document.getElementsByTagName("h1")[0].innerText = "Player 1 wins";
+// professors way
+  document.querySelector("h1")[0].innerText = "Player 1 wins";
 } else if (secondRandomNumber > randomNumber) {
   document.getElementsByTagName("h1")[0].innerText = "Player 2 wins";
 } else {
